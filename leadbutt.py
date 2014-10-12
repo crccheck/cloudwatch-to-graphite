c313cccd2974c43deded633aef25f6b2020eb912
crccheck <c@crccheck.com>
2014-10-12 15:18:51 -0500

    first attempt at getting cloudwatch info formatted for graphite

---
+++ leadbutt.py
@@ -0,0 +1,48 @@
+from calendar import timegm
+import datetime
+import sys
+
+import boto.ec2.cloudwatch
+import yaml
+
+
+region = 'us-east-1'  # TODO make this configurable
+
+
+def main():
+    # TODO add --config-file option, should also accept stdin
+    with open('config.yaml') as fp:
+        try:
+            config = yaml.load(fp)
+        except yaml.YAMLError as e:
+            sys.stderr.write(unicode(e))  # XXX python3
+            sys.exit(1)  # TODO document exit codes
+    # print config
+    conn = boto.ec2.cloudwatch.connect_to_region(region)
+    for metric in config['metrics']:
+        results = conn.get_metric_statistics(
+            60,  # minimum: 60
+            datetime.datetime.utcnow() - datetime.timedelta(seconds=60 * 5),
+            datetime.datetime.utcnow(),
+            metric['MetricName'],  # RequestCount
+            metric['Namespace'],  # AWS/ELB
+            metric['Statistics'],  # Sum
+            dimensions=metric['Dimensions'],
+            unit=metric['Unit'],
+        )
+        formatter = ('cloudwatch.%(Namespace)s.%(dimension)s.%(MetricName)s'
+            '.%(Statistics)s.%(Unit)s')
+        context = dict(
+            metric,
+            dimension=metric['Dimensions'].values()[0],
+        )
+        for result in results:
+            # get and then sanitize metric name
+            metric_name = (formatter % context).replace('/', '.').lower()
+            print metric_name,
+            print result[metric['Statistics']],
+            print timegm(result['Timestamp'].timetuple())
+
+
+if __name__ == '__main__':
+    main()
