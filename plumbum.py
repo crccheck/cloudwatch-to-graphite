f73de7e0cc25477b6c7ef351182ca619747000b6
crccheck <c@crccheck.com>
2014-10-20 23:51:50 -0500

    get code to return some resources

--- plumbum.py
+++ plumbum.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 """
 Usage:
-  plumbum <template> <namespace> [options]
+  plumbum <template> <namespace> [options]...
 
 Examples:
 
@@ -10,7 +10,8 @@ Examples:
 """
 from __future__ import unicode_literals
 
-from docopt import docopt
+import sys
+
 import boto.ec2
 import boto.ec2.elb
 import boto.rds
@@ -23,18 +24,10 @@ def get_property_func(key):
     Look for it as an attribute, and if that does not work, look to see if it
     is a tag.
     """
-    aliases = {
-        'ip': 'ip_address',
-        'private_ip': 'private_ip_address',
-    }
-    unaliased_key = aliases.get(key, key)
-
     def get_it(obj):
         try:
-            return getattr(obj, unaliased_key)
+            return getattr(obj, key)
         except AttributeError:
-            if key == 'name':
-                return obj.tags.get('Name')
             return obj.tags.get(key)
     return get_it
 
@@ -46,18 +39,13 @@ def filter_key(filter_args):
     return filter_instance
 
 
-def voyeur(instances, to_row, sort_by=None, filter_by=None):
-    if sort_by:
-        instances.sort(key=get_property_func(sort_by))
+def lookup(instances, filter_by=None):
     if filter_by:
-        instances = filter(filter_key(filter_by), instances)  # XXX overwriting original
-    return map(to_row, instances)
+        return filter(filter_key(filter_by), instances)
+    return instances
 
 
-def get_options(input_args, headers=None):
-    if headers is None:
-        headers = ()
-    sort_by = None  # WISHLIST have a tuple
+def get_options(input_args):
     filter_by_kwargs = {}
     for arg in input_args:
         if arg.startswith('-'):
@@ -65,88 +53,40 @@ def get_options(input_args, headers=None):
             continue
         if '=' in arg:
             key, value = arg.split('=', 2)
-            if key not in headers:
-                exit('{} not valid'.format(key))
             filter_by_kwargs[key] = value
-        elif arg in headers:
-            sort_by = arg
-        else:
-            print 'skipped', arg
-    return sort_by, filter_by_kwargs
+    return filter_by_kwargs
 
 
-def list_ec2(input_args):
-    headers = (
-        'name',
-        'environment',
-        'site',
-        'ip',
-        'private_ip',
-        'launch_time',
-        'id',
-    )
-    sort_by, filter_by_kwargs = get_options(input_args, headers)
+def list_ec2(filter_by_kwargs):
     conn = boto.ec2.connect_to_region('us-east-1')  # XXX magic constant
     instances = conn.get_only_instances()
-    to_row = lambda x: (
-        x.tags.get('Name'),
-        x.tags.get('environment'),
-        x.tags.get('site'),
-        x.ip_address,
-        x.private_ip_address,
-        x.launch_time.split('T', 2)[0],
-        x.id,
-    )
-    print tabulate(
-        voyeur(instances, to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs),
-        headers=headers)
+    print lookup(instances, filter_by=filter_by_kwargs)
 
 
-def list_elb(input_args):
-    headers = (
-        'name',
-        'dns_name',
-        'pool',  # not queryable/sortable
-        'created_time',
-    )
-    sort_by, filter_by_kwargs = get_options(input_args, headers)
+def list_elb(filter_by_kwargs):
     conn = boto.ec2.elb.connect_to_region('us-east-1')  # XXX magic constant
     instances = conn.get_all_load_balancers()
-    to_row = lambda x: (
-        x.name,
-        x.dns_name,
-        len(x.instances),
-        x.created_time,
-    )
-    print tabulate(
-        voyeur(instances, to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs),
-        headers=headers)
+    print lookup(instances, filter_by=filter_by_kwargs)
 
 
-def list_rds(input_args):
-    def to_row(x):
-        """More complicated `to_row` to handle missing DBName attribute."""
-        data = x.__dict__.copy()
-        data['DBName'] = getattr(x, 'DBName', '???')
-        return (
-            x.id,
-            '{engine}://{master_username}@{_address}:{_port}/{DBName}'.format(**data),
-        )
-    headers = (
-        'id',
-        # 'engine',
-        'uri',  # derived
-    )
-    sort_by, filter_by_kwargs = get_options(input_args, headers)
+def list_rds(filter_by_kwargs):
     conn = boto.rds.connect_to_region('us-east-1')
     instances = conn.get_all_dbinstances()
-    print tabulate(
-        voyeur(instances, to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs),
-        headers=headers)
+    print lookup(instances, filter_by=filter_by_kwargs)
 
 
 def main():
-    options = docopt(__doc__)
+    options = sys.argv[1:]
+    template = options[0]
+    namespace = options[1].lower()
+    filters = get_options(options[2:])
+    # should I be using ARNs?
+    if namespace in ('ec2', 'aws/ec2'):
+        list_ec2(filters)
+    if namespace in ('elb', 'aws/elb'):
+        list_elb(filters)
+    if namespace in ('rds', 'aws/rds'):
+        list_rds(filters)
     print options
 
 
