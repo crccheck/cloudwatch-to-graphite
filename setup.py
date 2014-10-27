baea648234aa87e004e8b4ae6aaea5c3f2a6105a
crccheck <c@crccheck.com>
2014-10-27 00:31:04 -0500

    bump version to v0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='cloudwatch-to-graphite',
     description='Helper for pushing AWS CloudWatch metrics to Graphite',
-    version='0.2.0',
+    version='0.3.0',
     author='Chris Chang',
     author_email='c@crccheck.com',
     url='https://github.com/crccheck/cloudwatch-to-graphite',
